c7f3fb73217dec6cb97da96e4d730117b881ba4a
Julia Michalska <michalskjul@gmail.com>
2021-01-31 03:39:14 +0100

    Created Loan and Payment models

--- LendingCompany.BL/Model/Loan.cs
+++ LendingCompany.BL/Model/Loan.cs
@@ -1,9 +1,29 @@
-﻿using LendingCompany.Domain.Model;
+﻿using System;
+using System.Collections.Generic;
+using LendingCompany.Domain.Model;
 
 namespace LendingCompany.BL.Model
 {
     public class Loan : Entity
     {
-
+        public Loan(int numberOfInstalments, double interest, double amount)
+        {
+            CreationDate = DateTime.UtcNow;
+            NumberOfInstalments = numberOfInstalments;
+            Interest = interest;
+            EstimatedRepaymentDate = CreationDate.AddMonths(numberOfInstalments);
+            Amount = amount;
+        }
+        public DateTime CreationDate { get; }
+        public DateTime EstimatedRepaymentDate { get; }
+        public int NumberOfInstalments { get; }
+        public double Amount { get; }
+        public double PaidAmount { get; set; }
+        public bool IsRepaid { get; set; }
+        public int TotalDaysOfDelay { get; set; }
+        public double Interest { get; set; }
+        public decimal PenaltyAmount { get; set; }
+        public DateTime? ActualRepaymentDate { get; set; }
+        public IList<Payment> Payments { get; set; }
     }
 }
---
+++ LendingCompany.BL/Model/Payment.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using LendingCompany.Domain.Model;
+
+namespace LendingCompany.BL.Model
+{
+    public class Payment : Entity
+    {
+        public Payment(double baseAmount, DateTime finalPaymentDate)
+        {
+            BaseAmount = baseAmount;
+            FinalPaymentDate = finalPaymentDate;
+        }
+
+        private double _paidAmount;
+        public double BaseAmount { get; }
+        public double PenaltyAmount { get; set; }
+        public double PaidAmount
+        {
+            get => _paidAmount;
+            private set
+            {
+                IsPaid = value >= BaseAmount;
+                _paidAmount = value;
+            }
+        }
+        public DateTime LastPaymentDate { get; private set; }
+        public DateTime FinalPaymentDate { get; }
+        public bool IsPaid { get; private set; }
+
+        public double Pay(double amount)
+        {
+            LastPaymentDate = DateTime.UtcNow;
+            var sumOfPayments = PaidAmount + amount;
+            var toPay = BaseAmount + PenaltyAmount;
+
+            if (sumOfPayments > toPay)
+            {
+                PaidAmount = BaseAmount + PenaltyAmount;
+                return sumOfPayments - BaseAmount;
+            }
+            PaidAmount = sumOfPayments;
+            return 0;
+        }
+    }
+}
